df1d3cc835a990347ad9fe61f189c2b97217684c
MR. FRANCIS ANDOH <andohfrancis9187@gmail.com>
2023-10-26 15:06:32 +0000

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,10 +1,12 @@
 class Student < Person
-  def initialize(age, id, classroom, name: 'Unknown', parent_permission: true)
-    super(age, id, name, parent_permission)
+
+  def initialize(age, classroom,name: "Unknown", parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
+
   def play_hooky
-    '¯(ツ)/¯'
+    return "¯\(ツ)/¯"
   end
 end
