ed598ed8e64a707aa45ebff8681de16b556c5251
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-10-27 14:40:34 +0000

    resolved linters error

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person < Nameable
   attr_reader :id, :rentals
   attr_accessor :name, :age, :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super
     @age = age
     @name = name
